c608c42e1c2053f7e1b9e7a5bb1be503bc50e7de
Namish Arora <namish.arora@gmail.com>
2023-10-16 15:19:43 -0400

    Basic Math and Data Types

---
+++ Basic_data_types.py
@@ -0,0 +1,16 @@
+#integer
+#float
+#strings
+#boolean
+
+
+#variable name = value
+
+number = 5
+
+print("\n---------------1----------------")
+print("Integer",number) #integer
+print("Float",float(number))
+print("String: 5")
+print(False)
+#True or False
---
+++ Basic_math.py
@@ -0,0 +1,61 @@
+
+# +
+# -
+# *
+# /
+
+#defining 2 variables x and y with 2 distinct values
+
+x = 6
+y = 3
+
+# to find results of 
+#x+y
+#x-y
+#x*y
+#x/y
+
+# 2 ways to compute the result
+# depends on what you are going to do with the result
+
+
+#1
+# to use the result later in the program or  
+print("---------------    1     ----------------")
+z = x+y
+print(z)
+z = x-y
+print(z)
+z = x*y
+print(z)
+z = x/y
+print(z)
+
+
+#2
+# to use the result calculated once or printing 
+#NOTE:- both the answers are correct , its just about what to you want to do with the result.
+print("\n\n---------------    2    ----------------")
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+
+
+
+print("\n\n\n The value of z is still",z ,"(the latest calculated value)")
+
+print("\n\n-----------   3   ------------")
+
+# % 
+# **
+num1 = 8
+# 8
+num2 = 4
+print("%:",num1%num2)
+
+
+num1 = 3
+num2 = 3
+print("**:",num1**num2)
